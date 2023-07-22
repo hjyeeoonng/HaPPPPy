83b7205200ada37fac9fb0f3f7f3d97b96ad6e57
taeyeon0319 <xodus1349@likelion.org>
2023-07-22 22:20:09 +0900

    [김태연]물품 정보 입력 삭제 api

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,6 +1,10 @@
 import express from 'express'; // module
+import pg from 'pg';
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from 'dotenv'
+
+dotenv.config(); // .env 파일에서 환경 변수를 읽어옴
 
 //variable
 const PORT = 5000;
@@ -12,15 +16,69 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 //routing
-let visit = 0;
-
-app.get("/", (req, res) => {
-    res.json({ "name": "HAPPPPY" })
+const pool = new pg.Pool({
+  host: process.env.DB_HOST, // 환경 변수에서 값 읽어옴
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-app.get("/hi", (req, res) => {
-    visit += 1;
-    res.json({ "message": "안녕하세요 햅삐입니다." })
+// 첫 번째 페이지에서 입력받은 값(수입/수출, 항공/해상) 저장
+let shipmentType = "";
+let transportType = "";
+
+app.post("/inputPage1", (req, res) => {
+  shipmentType = req.body.shipmentType;
+  transportType = req.body.transportType;
+  res.send("success");
+});
+
+// 물품 정보를 입력받는 두 번째 페이지
+app.post("/inputPage2", async (req, res) => {
+  const { item_name, hs_code, total_price, total_weight, width, height, depth } = req.body;
+
+  const client = await pool.connect();
+
+  try {
+    const query = "INSERT INTO products (type, transport, item_name, hs_code, total_price, total_weight, width, height, depth) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+    await client.query(query, [shipmentType, transportType, item_name, hs_code, total_price, total_weight, width, height, depth]);
+    res.send("success");
+  } catch (error) {
+    console.error("Error while inserting product data:", error);
+    res.status(500).send("Error while inserting product data.");
+  } finally {
+    client.release();
+  }
+});
+
+// 세 번째 페이지에서 입력한 값을 보여주는 화면
+app.get("/displayData", async (req, res) => {
+  const client = await pool.connect();
+
+  try {
+    const query = "SELECT * FROM products";
+    const result = await client.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error("Error while fetching product data:", error);
+    res.status(500).send("Error while fetching product data.");
+  } finally {
+    client.release();
+  }
+});
+
+// 입력 정보 삭제(뒤로가기/처음으로 돌아가기 실행 시 필요)
+app.delete('/delete', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    await client.query('DELETE FROM products');
+
+    client.release();
+
+    res.status(200).json({ message: '모든 물품 데이터가 삭제되었습니다.' });
+  } catch (error) {
+    res.status(500).json({ error: '물품 데이터 삭제 실패' });
+  }
 });
 
 // server start
