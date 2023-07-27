a1a52a564f1a80e773b7db53b5342deaac2c31e5
102sae <102chimee@gmail.com>
2023-07-27 13:32:23 +0900

    버튼 클릭 이벤트 수정

--- frontend/src/components/Select.jsx
+++ frontend/src/components/Select.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 const Root = styled.div`
@@ -23,7 +24,7 @@ const SelectBoxContainer = styled.div`
   padding: 10px 10px;
 `;
 
-const SelectBox = styled.div`
+const SelectBox1 = styled.button`
   width: 146px;
   height: 46px;
   display: flex;
@@ -32,21 +33,59 @@ const SelectBox = styled.div`
   border-radius: 10px;
   background: ${(props) => (props.color === 1 ? "#dddfe7" : "#595d62")};
   color: ${(props) => (props.color === 1 ? "#000" : "#fff")};
+  border: ${props => (props.isClicked ? '2px solid black' : '0px solid black')};
   text-align: center;
   font-family: Ubuntu;
   font-size: 18px;
   font-weight: 700;
   cursor: pointer;
+  
+  &:active {
+    border: 1px solid black
+  }
+`;
+
+const SelectBox2 = styled.button`
+  width: 146px;
+  height: 46px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: ${(props) => (props.color === 1 ? "#dddfe7" : "#595d62")};
+  color: ${(props) => (props.color === 1 ? "#000" : "#fff")};
+  border: ${props => (props.isClicked ? '2px solid black' : '0px solid black')};
+  text-align: center;
+  font-family: Ubuntu;
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+  
+  &:active {
+    border: 1px solid black
+  }
 `;
 
 export const Select = ({ question, option1, option2 }) => {
+  const [isClicked1, setIsClicked1] = useState(false);
+  const [isClicked2, setIsClicked2] = useState(false);
+
+  const handleClickButton1 = () => {
+    setIsClicked1((prev)=>!prev);
+  };
+
+  const handleClickButton2 = () => {
+    setIsClicked2((prev)=>!prev);
+  };
+
   return (
     <Root>
       <SelectText>{question}</SelectText>
       <SelectBoxContainer>
-        <SelectBox
+        <SelectBox1
           color={1}
           onClick={(e) => {
+            handleClickButton1();
             e.target.innerText === "수출"
               ? localStorage.setItem(
                   "shipmentType",
@@ -57,12 +96,14 @@ export const Select = ({ question, option1, option2 }) => {
                   JSON.stringify(e.target.innerText)
                 );
           }}
+          isClicked={isClicked1}
         >
           {option1}
-        </SelectBox>
-        <SelectBox
+        </SelectBox1>
+        <SelectBox2
           color={2}
           onClick={(e) => {
+            handleClickButton2();
             e.target.innerText === "수입"
               ? localStorage.setItem(
                   "shipmentType",
@@ -73,9 +114,10 @@ export const Select = ({ question, option1, option2 }) => {
                   JSON.stringify(e.target.innerText)
                 );
           }}
+          isClicked={isClicked2}
         >
           {option2}
-        </SelectBox>
+        </SelectBox2>
       </SelectBoxContainer>
     </Root>
   );
