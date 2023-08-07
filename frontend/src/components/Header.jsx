d48a95927b15103c45a22641a8fafecbf3b9d947
102sae <102chimee@gmail.com>
2023-08-07 15:21:10 +0900

    header 디자인 수정

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,42 +1,45 @@
 import styled from "styled-components";
+import { ReactComponent as Profile } from "../img/profile.svg";
 
 const Root = styled.div`
+  width: 100vw;
+  padding: 0 10vw;
+  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.04);
+
+`;
+
+const HeaderContainer = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 0 0 20px 0;
+  position: relative;
 `;
 
 const StyledHeader = styled.div`
-  padding: 3% 5%;
+
   font-size: 36px;
   font-weight: bold;
   font-style: italic;
   text-shadow: 2px 2px 2px gray;
-`;
+  text-align:end;
+  `;
 
-const HeaderContainer = styled.div`
-  width: 100vw;
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const HeaderSignIn = styled.div`
-  margin: 10px 10px;
-`;
-
-const HeaderLogin = styled.div`
-  margin: 10px 10px;
-`;
+const ProfileIcon = styled.div`
+  position: absolute;
+  top: 20%;
+  right: 25px;
+`
 
 export const Header = () => {
   return (
     <Root>
-      <StyledHeader>platform</StyledHeader>
       <HeaderContainer>
-        <HeaderSignIn>회원가입</HeaderSignIn>
-        <HeaderLogin>로그인</HeaderLogin>
+        <StyledHeader>LOGO</StyledHeader>
+        <ProfileIcon>
+        <Profile></Profile>
+        </ProfileIcon>
       </HeaderContainer>
     </Root>
   );
