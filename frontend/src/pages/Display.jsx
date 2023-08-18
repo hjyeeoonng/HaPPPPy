b0079838b4a0a6f32f4963a8e273006e03af1bbb
hjyeeoonng <100748980+hjyeeoonng@users.noreply.github.com>
2023-08-19 00:26:36 +0900

    현정 디자인 수정

--- frontend/src/pages/Display.jsx
+++ frontend/src/pages/Display.jsx
@@ -5,26 +5,28 @@ import React, { useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router";
 import axios from "axios";
+import theme from "../value/color";
 
 const Root = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 5px 5px;
+width: 375px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin: 0 auto;
+overflow-x: hidden;
 `;
 
 //물품명, 견적요청마감일, 견적평균가, 견적서 정렬
 const DisplayTextBox = styled.div`
-  margin-right: 30px;  
-  margin-left: 30px;
-  width: 100%;
-  height: 100%;
+  width: 328px;
+  height: 102px;
+  flex-shrink: 0;
+  border-radius: 5px;
+  background: var(--background, #F6F7F9);
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: flex-start;
 `
 
@@ -40,79 +42,100 @@ const DisplayTextBox2 = styled.div`
 `
 //물품명, 견적요청마감일, 견적평균가
 const DisplayText = styled.div`
-  width: 400px;
-  height: 34px;
-  flex-shrink: 0;
-  color: #000;
-  font-family: Inria Sans;
-  font-size: 18px;
-  font-style: bold;
+  color: var(--maintext, #1C1C1C);
+  font-family: AppleSDGothicNeoM00;
+  font-size: 14px;
+  font-style: normal;
   font-weight: 400;
   line-height: normal;
-  text-align: left;
+  letter-spacing: 0.1px;
 `
 
 const DisplayText2 = styled.div`
-  width: 100%;
+  color: var(--maintext, #1C1C1C);
+  font-family: AppleSDGothicNeoM00;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 125% */
+  letter-spacing: -0.5px;
   height: 34px;
   flex-shrink: 0;
-  color: #000;
-  font-family: Inria Sans;
-  font-size: 18px;
-  font-style: bold;
-  font-weight: 400;
-  line-height: normal;
   text-align: left;
 `
+const DisplayText3 = styled.div`
+  height: 34px;
+  flex-shrink: 0;
+  text-align: left;
+  color: var(--maintext, #1C1C1C);
+  font-family: AppleSDGothicNeoB00;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 111.111% */
+  letter-spacing: -0.5px;
+`
+
 //견적서
 const DisplayTextMain = styled.div`
-  margin-top: 12px;
-  height: 60px;
-  width: 120px;
-  color: #000;
-  font-family: Inria Sans;
-  font-size: 28px;
-  font-style: bold;
-  font-weight: 700;
-  line-height: normal;
+  margin : 18px; 
+  color: var(--maintext, #1C1C1C);
+  font-family: AppleSDGothicNeoB00;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: 20px; /* 83.333% */
+  letter-spacing: -0.5px;
 `
 //물품 변경하기
 const DisplayChangeButton = styled.div`
-  width: 292px;
-  height: 46px;
+  width: 327px;
+  height: 54px;
   display: flex;
   align-items: center;
   justify-content: center;
   position: relative;
   bottom: 8%;
   border-radius: 10px;
-  background: #dddfe7;
+  border:none;
+  background:${theme.main};
+  color: white;
   font-size: 18px;
-  font-weight: 700;
+  font-weight: 400;
+  &:hover {
+  background:${theme.sub};
+  }
+
+  &:active {
+  background: ${theme.main};
+  }
+
+  &:not(:active) {
+  transition: background 0.3s;
+  }
 `;
 
 const DisplayLine = styled.div`
-  margin-top:6px;
-  margin-bottom:6px;
-  width: 100%;
-  height: 1px;
-  background: #000;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background-color:#C4C4C4;
+  height: 0.5px;
+  margin:16px;
+  width: 292px;
+  stroke-width: 0.5px;
+  stroke: var(--unnamed, #C4C4C4);
 `
 
 const DisplayListBox = styled.div`
   padding: 0px;
   margin: 0px;
-  width: 100%;
-  height: 100%;
+  width: 296px;
+  height: 50px;
   display: flex;
   flex-direction: row;
   justify-content: center;
 `
 
 const DisplayListImg = styled.div`
-  padding: auto;
-  margin: auto;
+  margin-left:16px;
   width: 50px;
   height: 50px;
   flex-shrink: 0;
@@ -121,21 +144,35 @@ const DisplayListImg = styled.div`
 `
 
 const DisplayListButton = styled.div`
-  margin: auto;
-  width: 80%;
-  height: 40px;
-  border-radius: 10px;
-  background: #595D62;
-  color: #FFF;
+  width:116px;
+  height:24px;
+  display: inline-flex;
+  padding: 8px 16px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  border-radius: 8px;
+  background: var(--sub, #6C89FF);
+  color: var(--color-text-dark, #FFF);
   text-align: center;
-  font-family: Ubuntu;
-  font-size: 14px;
+
+  /* Mobile/Text/Small/Bold */
+  font-family: Montserrat;
+  font-size: 12px;
   font-style: normal;
-  font-weight: 700;
-  line-height: normal;
-  display : flex;
-  justify-content : center;
-  align-items : center;
+  font-weight: 600;
+  line-height: 19.5px; /* 150% */
+`
+
+const DisplayListWrap = styled.div`
+  overflow : auto;
+  margin-bottom:18px;
+  width: 328px;
+  height: 344px;
+  flex-shrink: 0;
+  border-radius: 10px;
+  border: 1px solid var(--unnamed, #C4C4C4);
+  background: var(--unnamed, #FFF);
 `
 
 export const Display = () => {
@@ -183,8 +220,9 @@ export const Display = () => {
           <DisplayText>물품명 마샬 스피커<br/></DisplayText>
           <DisplayText>견적요청마감일 2023-07-27<br/></DisplayText>
           <DisplayText>견적평균가 200000원<br/></DisplayText>
-          <DisplayTextMain>견적서</DisplayTextMain>
         </DisplayTextBox>
+        <DisplayTextMain>견적서</DisplayTextMain>
+        <DisplayListWrap>
         <DisplayLine></DisplayLine>
         {
           data.map((item, index)=>(
@@ -192,7 +230,7 @@ export const Display = () => {
             <DisplayListBox>
               <DisplayListImg></DisplayListImg>
               <DisplayTextBox2>
-                <DisplayText2>{item.name}</DisplayText2>
+                <DisplayText3>{item.name}</DisplayText3>
                 <DisplayText2>{item.price}</DisplayText2>
               </DisplayTextBox2>
               <DisplayListButton onClick={handleClick}>세부 정보</DisplayListButton>
@@ -201,6 +239,7 @@ export const Display = () => {
             </div>
           ))
         }
+        </DisplayListWrap>
         <DisplayChangeButton>물품 변경하기</DisplayChangeButton>
       </Root>
     );
